49c4b2cf0f6c62c1faff372c543ceb65db337ed1
nojes <vv.nojenko@gmail.com>
2016-10-27 12:08:50 +0300

    Simple search file example

---
+++ Lesson1/01-FileSearch/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01_FileSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//            Search();
+            Console.ReadLine();
+            Thread thread = new Thread(Search);
+
+            thread.Start();
+
+            Console.Read();
+        }
+
+        static void Search()
+        {
+            string ext = "*.";
+
+            Console.Write(" Input file extension > ");
+            ext += Console.ReadLine();
+
+            var root = new DirectoryInfo("D:/Nozhenko");
+            DirectoryInfo[] directories = root.GetDirectories();
+
+            Console.WriteLine();
+            foreach (DirectoryInfo directory in directories) {
+                FileInfo[] files = directory.GetFiles(ext);
+
+                if (files.Length != 0) {
+                    foreach (var file in files) {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"/{directory.Name}/{file.Name}");
+                        Console.ResetColor();
+                    }  
+                } else {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"/{directory.Name}/ {ext} not found.");
+                    Console.ResetColor();
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
