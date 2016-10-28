c1115d90c4c637d7284bd10f420a43150941799f
nojes <vv.nojenko@gmail.com>
2016-10-28 10:16:46 +0300

    Singleton

---
+++ Lesson2/04-Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.GetInstance();
+            var singleton2 = Singleton.GetInstance();
+            var singleton3 = Singleton.GetInstance();
+
+            Console.WriteLine($"singleton.HashCode: {singleton.GetHashCode()} \n");
+            Console.WriteLine($"singleton2.HashCode: {singleton2.GetHashCode()} \n");
+            Console.WriteLine($"singleton3.HashCode: {singleton3.GetHashCode()} \n");
+
+            var guid = Guid.NewGuid();
+            Console.WriteLine($"Guid: {guid}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson2/04-Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace _04_Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private static object _lockObj = new object();
+
+        public Singleton()
+        {
+//            Console.WriteLine("{0}", MethodBase.GetCurrentMethod());
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null) {
+                lock (_lockObj) {
+                    if (_instance == null) {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
