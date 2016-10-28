4bb05bab4a5dd4add4e1f7ac72859f3af7d8e113
nojes <vv.nojenko@gmail.com>
2016-10-28 09:40:02 +0300

    Timer examples

---
+++ Lesson2/01-Timer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01_Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimerCallback timerCallback = new TimerCallback(TimerTick);
+
+            Timer timer = new Timer(timerCallback);
+            timer.Change(2000, 500);
+
+            Console.ReadKey();
+        }
+
+        static void TimerTick(object o)
+        {
+            Timer t = o as Timer;
+
+            Console.WriteLine("tick");
+        }
+    }
+}
---
+++ Lesson2/02-Timer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02_Timer
+{
+    class Program
+    {
+        private static int sec = 10;
+
+        static void Main(string[] args)
+        {
+            TimerCallback timerCallback = new TimerCallback(TimerTick);
+            Timer timer = new Timer(timerCallback);
+
+            Console.WriteLine("Timer start.");
+            timer.Change(1000, 1000);
+
+            Console.ReadKey();
+        }
+
+        private static void TimerTick(object state)
+        {
+            sec--;
+            Console.WriteLine(sec.ToString());
+
+            if (sec > 0) return;
+
+            Timer t = (Timer)state;
+            t.Dispose();
+            Console.WriteLine("Timer stoped.");
+        }
+    }
+}
