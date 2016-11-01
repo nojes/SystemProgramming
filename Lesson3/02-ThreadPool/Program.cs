f6ca4ae5138459729ee63ef06e5222e891d3883b
nojes <vv.nojenko@gmail.com>
2016-11-01 11:17:32 +0200

    Threads pool

---
+++ Lesson3/02-ThreadPool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadHelper.ShowThreadsInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson3/02-ThreadPool/ThreadHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_ThreadPool
+{
+    public class ThreadHelper
+    {
+        public static void ShowThreadsInfo()
+        {
+            int availableWorkThreads, availableIOThreads, maxWorkThreads, maxIOThreads;
+            ThreadPool.GetAvailableThreads(out availableWorkThreads, out availableIOThreads);
+            ThreadPool.GetMaxThreads(out maxWorkThreads, out maxIOThreads);
+            Console.WriteLine($"Working threads: {availableWorkThreads} of {maxWorkThreads}");
+            Console.WriteLine($"Working IO threads: {availableIOThreads} of {maxIOThreads}");
+        }
+    }
+}
