de4642b5989ad6a631b84776e373bfdf319ec596
nojes <vv.nojenko@gmail.com>
2016-11-03 10:01:05 +0200

    threads lock

---
+++ Lesson4/01-Counter/Counter.cs
@@ -0,0 +1,40 @@
+﻿namespace _01_Counter
+{
+    public class Counter
+    {
+        private int count;
+        private int maxCount;
+        private object lockObj = new object();
+
+        public int Count => count;
+
+        public int MaxCount {
+            get { return maxCount; }
+            set { maxCount = value; }
+        }
+
+        public Counter()
+        {
+            maxCount = 1000000;
+        }
+
+        public Counter(int maxCount)
+        {
+            this.maxCount = maxCount;
+        }
+
+        public void UpdateFields()
+        {
+            for (int i = 0; i < maxCount; i++) {
+                lock(lockObj) {
+                    ++count;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $" Count: {count}";
+        }
+    }
+}
---
+++ Lesson4/01-Counter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int threadsCount = 0;
+            Counter c = new Counter();
+
+            Console.Write("Max counter value -> ");
+            c.MaxCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Threads count -> ");
+            threadsCount = Convert.ToInt32(Console.ReadLine());
+            
+
+            Thread[] threads = new Thread[threadsCount];
+            for (int i = 0; i < threads.Length; i++) {
+                threads[i] = new Thread(c.UpdateFields);
+                threads[i].Start();
+            }
+            foreach (Thread t in threads) {
+                t.Join();
+            }
+
+            Console.WriteLine(c.ToString());
+            Console.ReadKey();
+        }
+    }
+}
