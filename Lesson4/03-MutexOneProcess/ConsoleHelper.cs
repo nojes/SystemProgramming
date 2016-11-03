ff5b15001be0c1ce3ba69d31da4d201d2daea482
nojes <vv.nojenko@gmail.com>
2016-11-03 11:01:01 +0200

    Single application with Mutex

---
+++ Lesson4/03-MutexOneProcess/ConsoleHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Eventing;
+
+namespace _03_MutexOneProcess
+{
+    public class ConsoleHelper
+    {
+        public static void WriteError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\n {message}");
+            Console.ResetColor();
+        }
+
+        public static void WriteSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\n {message}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Lesson4/03-MutexOneProcess/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03_MutexOneProcess
+{
+    class Program
+    {
+        static Mutex m;
+        static string mutexName = "TEST_MUTEX";
+
+        static void Main(string[] args)
+        {
+            
+            try {
+                m = Mutex.OpenExisting(mutexName);
+            }
+            catch (Exception ex) {
+                ConsoleHelper.WriteError(ex.Message);
+            }
+
+            if (m != null) {
+                ConsoleHelper.WriteError("App is already running!");
+                Console.ReadKey();
+            }
+
+            using (m = new Mutex(true, mutexName)) {
+                ConsoleHelper.WriteSuccess("App is successfully running.");
+                Console.WriteLine("\n Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
