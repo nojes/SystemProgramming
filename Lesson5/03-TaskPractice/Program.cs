3e04f4c810b4c7e10875fb5a742d7a9ea1e7575d
nojes <vv.nojenko@gmail.com>
2016-11-08 12:04:30 +0200

    Task students example

---
+++ Lesson5/03-TaskPractice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03_TaskPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task<List<Student>> task = new Task<List<Student>>(getStudents, 10);
+            task.Start();
+            List<Student> students = task.Result;
+
+            foreach (Student student in students) {
+                student.ConsoleWrite();
+            }
+
+            Console.ReadKey();
+        }
+
+        static Student getStudent()
+        {
+            string[] firstnames = {
+                "Vasya",
+                "Kolya",
+                "Petya"
+            };
+
+            Random r = new Random();
+            Student student = new Student()
+            {
+                Firstname = firstnames[r.Next(firstnames.Length)],
+                Lastname = "Pupkin",
+                Age = 20
+            };
+
+            return student;
+        }
+
+        static List<Student> getStudents(object count)
+        {
+            List<Student> students = new List<Student>();
+            for (int i = 0; i < (int)count; i++) {
+                students.Add(getStudent());
+
+                Thread.Sleep(1000);
+
+                Console.WriteLine($"Student {students[i].Firstname} added.");
+            }
+            return students;
+        }
+    }
+}
---
+++ Lesson5/03-TaskPractice/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03_TaskPractice
+{
+    class Student
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public uint Age { get; set; }
+
+        public override string ToString()
+        {
+            return $" Firstname: {Firstname} \n Lastname: {Lastname} \n Age: {Age} \n";
+        }
+
+        public void ConsoleWrite()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
