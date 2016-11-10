35aedb31bf2b506b6e2293dd59bd1b0c65de9513
nojes <vv.nojenko@gmail.com>
2016-11-10 11:32:18 +0200

    IDisposable

---
+++ Lesson6/04-IDisposable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_IDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var wrapper = new ResourceWrapper()) {
+                Console.WriteLine(wrapper);
+            }
+            Console.WriteLine(new string('-', 30));
+            
+            var wrapper2 = new ResourceWrapper();
+            Console.WriteLine(wrapper2);
+
+            wrapper2.Dispose();
+            wrapper2.Dispose();
+            wrapper2.Dispose();
+            wrapper2.Dispose();
+
+            var wrapper3 = new ResourceWrapper();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson6/04-IDisposable/ResourceWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04_IDisposable
+{
+    public class ResourceWrapper : IDisposable
+    {
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            CleanUp(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~ResourceWrapper()
+        {
+            Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}]: Finalise.");
+            CleanUp(false);
+        }
+
+        private void CleanUp(bool clean)
+        {
+            if (!this.disposed)
+            {
+                if (clean)
+                {
+                    Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}]: Release resources.");
+                }
+                Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}]: Finalized.");
+            }
+            this.disposed = true;
+        }
+    }
+}
