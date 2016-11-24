4b5993af4f7f48c19ea726410f6c01f1c86e7003
nojes <vv.nojenko@gmail.com>
2016-11-24 11:54:00 +0200

    ResetEvent

---
+++ Lesson7/01-ResetEvent/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace _01_ResetEvent
+{
+    class Program
+    {
+        private static readonly AutoResetEvent AutoReset = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any key ...");
+
+            Thread thread = new Thread(actionColors);
+            thread.Start();
+
+            Console.ReadKey();
+            AutoReset.Set();
+
+            Console.ReadKey();
+            AutoReset.Set();
+
+            Console.ReadKey();
+        }
+
+        private static void actionColors()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red");
+            AutoReset.WaitOne();
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow");
+            AutoReset.WaitOne();
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green");
+        }
+    }
+}
---
+++ Lesson7/02-ManualResetEvent/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace _02_ManualResetEvent
+{
+    class Program
+    {
+        private static readonly ManualResetEvent ManualReset = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any key ...");
+
+            Thread[] threads = {
+                new Thread(Action1),
+                new Thread(Action2), 
+            };
+
+            foreach (Thread thread in threads) {
+                thread.Start();
+            }
+
+            Console.ReadKey();
+            ManualReset.Set();
+
+
+            Console.ReadKey();
+        }
+
+        private static void Action1()
+        {
+            Console.WriteLine("Thread 1 is running.");
+            ManualReset.WaitOne();
+            Console.WriteLine("Thread 1 is stoped.");
+        }
+
+        private static void Action2()
+        {
+            Console.WriteLine("Thread 2 is running.");
+            ManualReset.WaitOne();
+            Console.WriteLine("Thread 2 is stoped.");
+        }
+    }
+}
