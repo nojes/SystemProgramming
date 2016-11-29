fdadd3b779237bc38bfb833ccd0d959a9256bf8f
nojes <vv.nojenko@gmail.com>
2016-11-29 10:05:47 +0200

    Async await: WebClient

---
+++ Lesson9/01-WebClient.WinForms/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _01_WebClient.WinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.richTextData = new System.Windows.Forms.RichTextBox();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textURL
+            // 
+            this.textURL.Location = new System.Drawing.Point(47, 19);
+            this.textURL.Name = "textURL";
+            this.textURL.Size = new System.Drawing.Size(513, 20);
+            this.textURL.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.btnLoadData);
+            this.groupBox.Controls.Add(this.richTextData);
+            this.groupBox.Controls.Add(this.label1);
+            this.groupBox.Controls.Add(this.textURL);
+            this.groupBox.Location = new System.Drawing.Point(12, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(566, 382);
+            this.groupBox.TabIndex = 2;
+            this.groupBox.TabStop = false;
+            // 
+            // richTextData
+            // 
+            this.richTextData.Location = new System.Drawing.Point(47, 45);
+            this.richTextData.Name = "richTextData";
+            this.richTextData.Size = new System.Drawing.Size(513, 294);
+            this.richTextData.TabIndex = 2;
+            this.richTextData.Text = "";
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(467, 345);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(93, 31);
+            this.btnLoadData.TabIndex = 3;
+            this.btnLoadData.Text = "Load data";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 401);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(590, 22);
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(67, 17);
+            this.toolStripStatusLabel.Text = "Status label";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 423);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.groupBox);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.RichTextBox richTextData;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+    }
+}
+
---
+++ Lesson9/01-WebClient.WinForms/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_WebClient.WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            textURL.Text = "https://habrahabr.ru/";
+            statusStrip.Items["toolStripStatusLabel"].Text = "Ready";
+        }
+
+        private async void btnLoadData_Click(object sender, EventArgs e)
+        {
+            await LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            try {
+                statusStrip.Items["toolStripStatusLabel"].Text = "Loading...";
+                var client = new WebClient { Encoding = Encoding.UTF8 };
+
+                //richTextData.Text = client.DownloadString(textURL.Text);
+
+                richTextData.Text = await client.DownloadStringTaskAsync(textURL.Text);
+                statusStrip.Items["toolStripStatusLabel"].Text = "Successfully loaded.";
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+    }
+}
